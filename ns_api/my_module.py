c871c7836f5ac4f0b9f5c2e16c162fb722b65835
FutraTino <63669705@qq.com>
2023-08-04 11:35:18 +0000

    修复逗号bug与路径不存在bug

--- ns_api/my_module.py
+++ ns_api/my_module.py
@@ -7,32 +7,40 @@ from config import *
 import utils_bucket
 import shutil
 # config
-cwd = Path('/nerfstudio')
+cwd = Path('/nerfstudio_restful')
 data_parent_dir = cwd / f'data/nerfstudio'
+
 output_dir = cwd / f'outputs'
 
-# test_meta
-test_slug = 'future-eraLi-123456'
-slug = test_slug
-data_dir = data_parent_dir / f'{slug}'
-video_path = data_dir / f'{slug}.mp4'
+# # test_meta
+# test_slug = 'future-eraLi-123456'
+# slug = test_slug
+# data_dir = data_parent_dir / f'{slug}'
+
 
 def download_video_to_dir_from_bucket(slug):
     key = f'{slug}.mp4'
-    filename = data_parent_dir / f'{slug}.mp4'
+    filename = data_parent_dir / slug /f'{slug}.mp4' # be careful
     utils_bucket.download_to_local(key,filename)
     return filename
 def create_nerf(slug):
-    info = {
-        'status':'downloading',
-        'latest_run_status':'downloading',
-        'latest_run_current_stage':'downloading',
-    }
-    utils_db.update_capture(slug,**info) # 下载中
-    download_video_to_dir_from_bucket(slug)
+    # mkdir
+    data_dir = data_parent_dir / f'{slug}'
+    video_path = data_dir / f'{slug}.mp4'
+    data_dir.mkdir(parents=True, exist_ok=True)
+    if not video_path.exists():
+    # 0. Download video from bucket
+        info = {
+            'status':'downloading',
+            'latest_run_status':'downloading',
+            'latest_run_current_stage':'downloading',
+        }
+        utils_db.update_capture(slug,**info) # 下载中
+        download_video_to_dir_from_bucket(slug)
 
     # 1. Started processing
     info = {
+        'status':'Started'
         'latest_run_status':'Started',
         'latest_run_current_stage': 'Preprocessing',
     }
@@ -46,7 +54,7 @@ def create_nerf(slug):
     }
     utils_db.update_capture(slug,**info)
     subprocess.run(f"ns-train nerfacto --data {data_dir}  --output-dir {output_dir} --pipeline.model.predict-normals True \
-        --max-num-iterations {200} --save-only-latest-checkpoint True --vis tensorboard  ",shell=True,cwd='/nerfstudio')
+        --max-num-iterations {200} --save-only-latest-checkpoint True --vis tensorboard  ",shell=True,cwd='/nerfstudio_restful')
 
     # 3. Start exporting mesh
     specific_output_dir = cwd / 'outputs' / slug
